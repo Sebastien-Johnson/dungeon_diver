2111a5595a8053b4a9df064225e5253ea62ae291
Sebastien-Johnson <sebastien.z.johnson@gmail.com>
2026-03-08 23:19:39 -0400

    Fleshed out equipment and items

---
+++ systems/stats.py
@@ -0,0 +1,10 @@
+class Stats():
+    def __init__(self, health=0, phys_armor=0, mag_armor=0, agility=0, mana=None, mag_pow=None, stamina=None, phys_pow=None):
+        self.health = health
+        self.phys_armor = phys_armor
+        self.mag_armor = mag_armor
+        self.agility = agility
+        self.mana = mana
+        self.mag_pow = mag_pow
+        self.stamina = stamina
+        self.phys_pow = phys_pow
