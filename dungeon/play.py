02c0593550a9d8a4d34341e3d4b7ae5f8a863518
Sebastien-Johnson <sebastien.z.johnson@gmail.com>
2026-03-09 22:48:35 -0400

    updated game class

---
+++ dungeon/play.py
@@ -0,0 +1,55 @@
+from systems.units import Unit
+
+class Game():
+    def __init__(self):
+        pass
+
+    def game_start(self):
+        print("Welcome.. to the dungeon.\n")
+        player1 = self.create_adventurer()
+        self.start_dungeon(player1)
+
+    def create_adventurer(self):
+        name = self.get_name
+        race = self.check_race()
+        player_class = self.check_class()
+
+        adventurer = Unit(name, race, player_class)
+        return adventurer
+    
+
+    def get_name(self):
+        print("What's your name adventurer?\n")
+        name = input()
+        return name
+
+    def check_race(self):
+        valid_races = ["human", "dwarf", "elf", "goliath"]
+        known_race = False
+        print("What race are you?\n")
+        player_race = input()
+
+        while known_race == False:
+            if player_race.lower() in valid_races:
+                known_race = True
+                return player_race
+            else:
+                print(f"Sorry, '{player_race}' is not a known race. Try again.\n")
+                player_race = input()
+    
+    def check_class(self):
+        valid_classes = ["warrior", "mage", "cleric", "ranger"]
+        propper_class = False
+        print("What class are you?\n")
+        player_class = input()
+
+        while propper_class == False:
+            if player_class.lower() in valid_classes:
+                propper_class = True
+                return player_class
+            else:
+                print(f"Sorry, '{player_class}' is not a propper class. Try again.\n")
+                player_class = input()
+
+    def start_dungeon(self):
+        pass
